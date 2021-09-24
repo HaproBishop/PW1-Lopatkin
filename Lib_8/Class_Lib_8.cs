3c90cd2b983c959c908eedafab87b14861191d73
HaproBishop <haprobishop@yandex.ru>
2021-09-24 23:02:39 +0300

    - Полная работа с библиотекой. (Никаких расчетов в xaml.cs) - Изменена логика работы программы - Упрощен код (Путь к сеньору :D)

--- Lib_8/Class_Lib_8.cs
+++ Lib_8/Class_Lib_8.cs
@@ -16,10 +16,29 @@ namespace Lib_8
 {
     public class Class_Lib_8
     {
-        public static void Find_MaxValue(int value1, int value2, ref int maxvalue)
+        public static int maxvalue;
+        public static string Find_MaxValue(int n)
         {
-            if (maxvalue < value1) maxvalue = value1;
-            if (maxvalue < value2) maxvalue = value2;
+            maxvalue = 0;
+            Random rnd = new Random();
+            int value1 = 0, value2 = 0;
+            string values = "";
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2.0 == 0) //Условие - проверка четности порядка числа (например, 2 - остатка не будет(0), следовательно - четное число при делении на 2)
+                { //Задавание первого числа и его добавление в строку 
+                    value1 = rnd.Next(0, n);
+                    values += Convert.ToString(value1) + " ";
+                }
+                if (i % 2.0 != 0) //Условие - проверка четности порядка числа (например, 3 - остаток будет(1), следовательно - нечетное число при делении на 2)
+                {//Задавание второго числа и его добавление в строку
+                    value2 = rnd.Next(0, n);
+                    values += Convert.ToString(value2) + " ";
+                }
+                if (value1 > maxvalue) maxvalue = value1; //Сравнение числа и максимума
+                if (value2 > maxvalue) maxvalue = value2;
+            }            
+            return values;
         }
     }
 }
--- PW1/MainWindow.xaml.cs
+++ PW1/MainWindow.xaml.cs
@@ -29,52 +29,34 @@ namespace PW1
         {
             InitializeComponent();
         }
-        int maxvalue;
+
         private void AboutProgram_Button_Click(object sender, RoutedEventArgs e)
         {//Вывод задания и разработчика на экран :D
             MessageBox.Show("Лопаткин Сергей Михайлович (Hapro Bishop). Практическая работа №1. Задание №8. Найти максимум из n целых случайных чисел X, распределенных в диапазоне от 0 до n.Вывести на экран на одной строке сгенерированные числа, на другой строкерезультат.", "О программе", MessageBoxButton.OK, MessageBoxImage.Warning);
         }
 
         private void SetRange_Button_Click(object sender, RoutedEventArgs e)
-        { 
-            maxvalue = 0; //Начальная очистка данных после нажатия клавиши диапазона
-            Random_TextBox.Clear();//
-            Result_TextBox.Clear();//
-            Random rnd = new Random();
-            bool prv = Int32.TryParse(Range_TextBox.Text, out int n); //Преобразование параметра Range_TextBox.Text в выходной параметр n с типом int 
-            int value1 = 0, value2 = 0; //Ввод переменных для 2-х чисел 
-
+        {                  
+            Random_TextBox.Clear();
+            Result_TextBox.Clear();
+            bool prv = Int32.TryParse(Range_TextBox.Text, out int n); //Преобразование параметра Range_TextBox.Text в выходной параметр n с типом int             
             //Ввод новой переменной - "n", являющаяся диапазоном чисел, необходимых сгенерировать
             if (prv == true) //Проверка на успешность преобразования параметра Range_TextBox.Text, иначе - задание не будет выполняться
             {
-                for (int i = 0; i < n; i++)
-                {
-                    if (i % 2.0 == 0) //Условие - проверка четности порядка числа (например, 2 - остатка не будет(0), следовательно - четное число при делении на 2)
-                    { //Задавание первого числа и его добавление в строку 
-                        value1 = rnd.Next(0, n);
-                        Random_TextBox.Text += Convert.ToString(value1) + " "; 
-                    }
-                    if (i % 2.0 != 0) //Условие - проверка четности порядка числа (например, 3 - остаток будет(1), следовательно - нечетное число при делении на 2)
-                    {//Задавание второго числа и его добавление в строку
-                        value2 = rnd.Next(0, n);
-                        Random_TextBox.Text += Convert.ToString(value2) + " ";
-                    }
-                    Class_Lib_8.Find_MaxValue(value1, value2, ref maxvalue); //Обращение к функции, находящейся в библиотеке Lib_8
-                }                
-            }
+                Random_TextBox.Text = Class_Lib_8.Find_MaxValue(n);
+            }                            
         }
 
         private void Range_TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {//Очистка значений, чтобы не путать пользователя
             Random_TextBox.Clear();
             Result_TextBox.Clear();
-            maxvalue = 0;
         }
 
         private void Find_Button_Click(object sender, RoutedEventArgs e)
-        {//Добавление результата в строку, если пользователь захочет увидеть результат поиска
-            Result_TextBox.Text = Convert.ToString(maxvalue);
+        {//Добавление результата в строку, если пользователь захочет увидеть результат поиска            
             if (Random_TextBox.Text == "" || Random_TextBox.Text == null) MessageBox.Show("Сначала создайте список случайных чисел!","ВНИМАНИЕ!", MessageBoxButton.OK, MessageBoxImage.Error);
+            else Result_TextBox.Text = Convert.ToString(Class_Lib_8.maxvalue);
         }
 
         private void Exit_Button_Click(object sender, RoutedEventArgs e)
